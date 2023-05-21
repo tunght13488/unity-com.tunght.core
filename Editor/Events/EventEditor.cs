510f587e0adc0518c236bf8a4dd33cb7ee993b11
tungha-sl <tungha.t@secretlab.sg>
2023-05-22 00:00:45 +0800

    feat: game event

---
+++ Editor/Events/EventEditor.cs
@@ -0,0 +1,22 @@
+﻿using TungHT.Core.Events;
+using UnityEditor;
+using UnityEngine;
+
+namespace TungHT.Core.Editor.Events {
+    [CustomEditor(typeof(GameEvent), true)]
+    public class EventEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            var e = target as GameEvent;
+
+            if (GUILayout.Button("Raise"))
+            {
+                e.Raise();
+            }
+        }
+    }
+}
---
+++ Runtime/Events/GameEventListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace TungHT.Core.Events {
+    public class GameEventListener : MonoBehaviour {
+        [Tooltip("Event to register with.")] public GameEvent @event;
+
+        [Tooltip("Response to invoke when Event is raised.")] public UnityEvent response;
+
+        private void OnEnable()
+        {
+            @event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            @event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            response.Invoke();
+        }
+    }
+}
