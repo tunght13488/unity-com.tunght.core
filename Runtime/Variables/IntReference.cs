d1364bdbc5da161405072e858832e264748dabba
tungha-sl <tungha.t@secretlab.sg>
2023-06-03 11:28:54 +0800

    feat: add IntReference

---
+++ Runtime/Variables/IntReference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TungHT.Core.Variables {
+    [Serializable]
+    public class IntReference {
+        public bool useConstant = true;
+        public int constantValue;
+        public IntVariable variable;
+
+        public IntReference()
+        {
+        }
+
+        public IntReference(int value)
+        {
+            useConstant = true;
+            constantValue = value;
+        }
+
+        public int Value => useConstant ? constantValue : variable.value;
+
+        public static implicit operator int(IntReference reference) => reference.Value;
+    }
+}
---
+++ Runtime/Variables/IntVariable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace TungHT.Core.Variables {
+    [CreateAssetMenu(fileName = "NewIntVariable", menuName = "Variables/IntVariable")]
+    public class IntVariable : ScriptableObject {
+#if UNITY_EDITOR
+        [Multiline] public string DeveloperDescription = "";
+#endif
+        public int value;
+
+        public void SetValue(int newValue)
+        {
+            value = newValue;
+        }
+
+        public void SetValue(IntVariable newValue)
+        {
+            value = newValue.value;
+        }
+
+        public void ApplyChange(int amount)
+        {
+            value += amount;
+        }
+
+        public void ApplyChange(IntVariable amount)
+        {
+            value += amount.value;
+        }
+    }
+}
